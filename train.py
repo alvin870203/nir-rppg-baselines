42c9e49ec858878aabc480c55abd49908240dd79
alvin870203 <alvin870203@gmail.com>
2023-12-11 05:01:38 +0800

    Fix grad accum bug

--- train.py
+++ train.py
@@ -328,15 +328,15 @@ while True:
             nir_imgs, ppg_signals = nir_imgs.to(device), ppg_signals.to(device)
         # backward pass, with gradient scaling if training in fp16
         scaler.scale(loss).backward()
-        # clip the gradient
-        if grad_clip != 0.0:
-            scaler.unscale_(optimizer)
-            torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
-        # step the optimizer and scaler if training in fp16
-        scaler.step(optimizer)
-        scaler.update()
-        # flush the gradients as soon as we can, no need for this memory anymore
-        optimizer.zero_grad(set_to_none=True)
+    # clip the gradient
+    if grad_clip != 0.0:
+        scaler.unscale_(optimizer)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+    # step the optimizer and scaler if training in fp16
+    scaler.step(optimizer)
+    scaler.update()
+    # flush the gradients as soon as we can, no need for this memory anymore
+    optimizer.zero_grad(set_to_none=True)
 
 
     # timing and logging
